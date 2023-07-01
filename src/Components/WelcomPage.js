a19feb3363d8ac47a73090e42808068fb2abffd3
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2023-07-01 12:25:21 +0330

    add welcom page

--- src/Components/WelcomPage.js
+++ src/Components/WelcomPage.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import pic1 from "../Assets/Images/pic1.png"
+import { Link } from 'react-router-dom'
 
 export default class WelcomPage extends Component {
-  render() {
-    return (
-      <div>WelcomPage</div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <img src={pic1} className="App-logo" alt="logo" />
+                <p>
+                    Welcom to p2p-messenger!
+                </p>
+                <Link className="App-link" to={"/set_att"}>Start New Conversation</Link>
+                
+            </div>
+        )
+    }
 }
